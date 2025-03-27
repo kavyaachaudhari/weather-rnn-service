b44edd71173518d879f61493cfc2a02fa46ccfb2
Kavya Chaudhari <107391676+kavyaachaudhari@users.noreply.github.com>
2025-03-26 20:57:04 -0400

    Update model.py

--- model.py
+++ model.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 class RNNModel(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
         super(RNNModel, self).__init__()
-        self.rnn = nn.RNN(input_size=2, hidden_size=64, batch_first=True)
+        self.rnn = nn.RNN(input_size=2, hidden_size=20, batch_first=True)
         self.fc = nn.Linear(64, output_size)
 
     def forward(self, x):
